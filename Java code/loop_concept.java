249d2d956f4ce9994e27a224e81c0a745759b585
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-01-07 12:24:46 +0530

    push done

---
+++ Java code/loop_concept.java
@@ -0,0 +1,20 @@
+public class loop_concept {
+    public static void main(String[] args) {
+        // This is the concept related to for loop;
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        // This is the concept about do while loop
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 10);
+        // This is the concept related to while loop
+        int j = 0;
+        while (j < 10) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
