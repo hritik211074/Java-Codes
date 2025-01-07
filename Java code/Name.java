b0b1b77cbbcb4bac26cb4bc8087bf1a172ce91c9
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-01-07 12:46:28 +0530

    Code push Done

---
+++ Java code/Name.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class Name{
+       public static void main(String args[]){
+           String name="Parag";
+           int len=0;
+           for(int i=0;i<name.length();i++){
+              len++;
+         }
+           System.out.println(len);
+     }
+ }
