a654f649ef6e301e8d52fafaee5a384826bcc901
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-01-07 12:18:35 +0530

    Java folder done

---
+++ Java code/Mainclass.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+ class Mainclass{
+ public static void main(String []args){
+       try{
+        String name =args[0];
+        System.out.println("My name is ->"+name);
+        String title=args[1];}catch(Exception e){}
+      
+
+       System.out.println("Hello World to guys");
+       Scanner sc=new Scanner(System.in);
+       int age =sc.nextInt();
+       if(age>=18) System.out.println("we can vote for election");
+       else System.out.println("we cannot vote in election");
+
+       
+   
+
+   }
+}
